f139e7a2dbb6b5d3f79884afc6bec4ba1e836405
shaobo10 <shaobo10@staff.sina.com.cn>
2018-09-01 15:13:57 +0800

    fjeow

---
+++ Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPod1
+//
+//  Created by shaobo10 on 2018/9/1.
+//  Copyright © 2018年 sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Classes/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyPod1
+//
+//  Created by shaobo10 on 2018/9/1.
+//  Copyright © 2018年 sina. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
